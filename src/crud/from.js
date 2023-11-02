5713c4216b275b7478f95f6ac240ca1023200a56
muthuraman <muthuraman.manickam@finstein.ai>
2023-11-02 13:00:56 +0530

    changes will add into a sign page

--- src/crud/from.js
+++ src/crud/from.js
@@ -17,7 +17,13 @@ const MyFormbuild = () => {
 
   const validationSchema = yup.object().shape({
     email: yup.string().email('Invalid email address').required('Email is required'),
-    password: yup.string().required('Password is required'),
+    password: yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+      'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character'
+    )
+    .required('Password is required'),
   });
 
   const formik = useFormik({
@@ -34,7 +40,8 @@ const MyFormbuild = () => {
             <div className='form-group'>
               <div className='login-form'>
                 <div className='form-group'>
-                  <label className='form-label'>Email :</label>
+                  <label className='form-label required'>Email </label>
+                  <span>:</span>
                   <div className='form-input'>
                     <Input
                       placeholder='Email'
@@ -44,20 +51,25 @@ const MyFormbuild = () => {
                       onChange={formik.handleChange}
                       onBlur={formik.handleBlur}
                     />
-                    {formik.touched.email}
+                    {formik.touched.email && formik.errors.email? 
+                    <div className='error'>{formik.errors.email}</div>:null }
                   </div>
                 </div>
                 <div className='form-group'>
-                  <label className='form-label'>Password :</label>
+                  <label className='form-label required'>Password </label>
+                  <span>:</span>
                   <div className='form-input'>
                     <Input
                       placeholder='Password'
-                      id='password' // Change to 'password'
+                      id='password'
                       name="password"
                       onChange={formik.handleChange}
                       onBlur={formik.handleBlur}
                       value={formik.values.password}
                     />
+                    {formik.touched.password && formik.errors.password ? (
+                    <div className="error">{formik.errors.password}</div>
+                    ) : null}
                   </div>
                 </div>
                 <Button className='form-btn' type='submit'>Login</Button>
